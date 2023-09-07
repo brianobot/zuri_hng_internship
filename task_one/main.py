e46f57f66e5384e6abcc377cb9b0775c38960e6f
Brian Obot <brianobot9@gmail.com>
2023-09-07 10:19:51 +0100

    Update timezone format

--- task_one/main.py
+++ task_one/main.py
@@ -11,7 +11,7 @@ async def home(slack_name: str = "Brian Obot", track: str = "backend"):
     return {
         "slack_name": slack_name,
         "current_day": now.strftime("%A"),
-        "utc_time": datetime.now(timezone.utc),
+        "utc_time": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
         "track": track,
         "github_file_url": "https://github.com/brianobot/zuri_hng_internship/blob/master/task_one/main.py",
         "github_repo_url": "https://github.com/brianobot/zuri_hng_internship",
