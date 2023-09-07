cb6bcc847d75ec3b3484c5b68dd11b808b2d14cf
Brian Obot <brianobot9@gmail.com>
2023-09-07 10:35:18 +0100

    Refactor codebase

--- task_one/main.py
+++ task_one/main.py
@@ -5,6 +5,9 @@ from datetime import datetime, timezone
 app = FastAPI()
 
 
+GITHUB_REPO_URL = "https://github.com/brianobot/zuri_hng_internship"
+
+
 @app.get("/api")
 async def home(slack_name: str = "Brian Obot", track: str = "backend"):
     now = datetime.now()
@@ -14,6 +17,6 @@ async def home(slack_name: str = "Brian Obot", track: str = "backend"):
         "utc_time": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
         "track": track,
         "github_file_url": "https://github.com/brianobot/zuri_hng_internship/blob/master/task_one/main.py",
-        "github_repo_url": "https://github.com/brianobot/zuri_hng_internship",
+        "github_repo_url": GITHUB_REPO_URL,
         "status_code": 200,
     }
