4b12ad4f5db71ad126f9c6cba7b955d2bde36c5f
Brian Obot <brianobot9@gmail.com>
2023-09-19 20:19:58 +0100

    Update progress with the hng internship program

--- task_one/main.py
+++ task_one/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-
 from datetime import datetime, timezone
 
 app = FastAPI()
 
-
 GITHUB_REPO_URL = "https://github.com/brianobot/zuri_hng_internship"
+TASK_ONE_FILE_URL = "https://github.com/brianobot/zuri_hng_internship/blob/master/task_one/main.py"
 
 
 @app.get("/api")
@@ -16,7 +15,7 @@ async def home(slack_name: str = "Brian Obot", track: str = "backend"):
         "current_day": now.strftime("%A"),
         "utc_time": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
         "track": track,
-        "github_file_url": "https://github.com/brianobot/zuri_hng_internship/blob/master/task_one/main.py",
+        "github_file_url": TASK_ONE_FILE_URL,
         "github_repo_url": GITHUB_REPO_URL,
         "status_code": 200,
     }
